451315888cfa36bcec9a1eb67fe5a447f872be7c
ruth.palomino@dharbor.com <ruth.palomino@dharbor.com>
2018-09-17 19:38:29 -0400

    New angular

--- moviesAngular/angular-movies-series-music/src/app/app.module.ts
+++ moviesAngular/angular-movies-series-music/src/app/app.module.ts
@@ -1,23 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {AppComponent } from './app.component';
-import {MoviesModule} from './movies/movies.module';
-import {MusicModule} from './music/music.module';
-import {SeriesModule} from './series/series.module';
-import {DiscsModule} from './discs/discs.module';
-
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    MoviesModule,
-    MusicModule,
-    SeriesModule,
-    DiscsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
